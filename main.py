edb5093f8a9982d09136602f2cfc61e1137834fe
Khalil <khalilr432@gmail.com>
2021-12-30 19:40:21 -0500

    added nedded error handling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException, ElementNotInteractableException, NoSuchElementException
 import json
 import time
 import re
@@ -47,8 +47,8 @@ class EasyApply:
         search_location.send_keys(Keys.RETURN)
         """
     def clickeasyapply(self):
-        time.sleep(1)
-        click_easyapply = self.driver.find_element_by_xpath("/html/body/div[6]/div[3]/div[3]/section/div/div/div/ul/li[8]/div/button")
+        time.sleep(2)
+        click_easyapply = self.driver.find_element_by_xpath("/html/body/div[5]/div[3]/div[3]/section/div/div/div/ul/li[8]/div/button")
         click_easyapply.click()
         time.sleep(1)
     def find_offers(self): 
@@ -67,15 +67,16 @@ class EasyApply:
             hover.perform()
             titles = result.find_elements_by_class_name('job-card-search__title.artdeco-entity-lockup__title.ember-view')
             for title in titles:
-                self.submit_apply(title)
+                self.submit(title)
                 
         if total_results_int > 24:
             time.sleep(2)
 
             # find the last page and construct url of each page based on the total amount of pages
-            find_pages = self.driver.find_elements_by_class_name("artdeco-pagination__pages atdeco-pagination__pages--number")
+            find_pages = self.driver.find_elements_by_class_name("artdeco-pagination__indicator.artdeco-pagination__indicator--number.ember-view")
             total_pages = find_pages[len(find_pages)-1].text
             total_pages_int = int(re.sub(r"[^\d.]", "", total_pages))
+            print(total_pages_int)
             get_last_page = self.driver.find_element_by_xpath("//button[@aria-label='Page "+str(total_pages_int)+"']")
             get_last_page.send_keys(Keys.RETURN)
             time.sleep(2)
@@ -89,21 +90,35 @@ class EasyApply:
                 results_ext = self.driver.find_elements_by_class_name("jobs-search-results__list-item.occludable-update.p0.relative.ember-view")
                 for result_ext in results_ext:
                     hover_ext = ActionChains(self.driver).move_to_element(result_ext)
-                    hover_ext.perform()
-                    titles_ext = result_ext.find_elements_by_class_name('job-card-search__title.artdeco-entity-lockup__title.ember-view')
+                   
+                    titles_ext = result_ext.find_elements_by_class_name('disabled.ember-view.job-card-container__link.job-card-list__title')
+                    print(titles_ext)
                     for title_ext in titles_ext:
+                       
                         self.submit(title_ext)
+                    hover_ext.perform()
+                   
         else:
             self.close_session()
     def submit(self,job_ad):
-        print("you are applying to the position of: ", job_ad.text())
-        job_ad.click()
-        time.sleep(1)
+        time.sleep(2)
+        try:
+            job_ad.click()
+        except ElementClickInterceptedException:
+            time.sleep(10)
+            discard_popup = self.driver.find_element_by_class_name("artdeco-toast-item__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--1.artdeco-button--tertiary.ember-view")
+            hover = ActionChains(self.driver).move_to_element(discard_popup)
+            hover.perform()
+            discard_popup.send_keys(Keys.RETURN)
+            print("discarding popup")
+        time.sleep(2)
         
         #click on apply
         try:
-            in_apply = self.driver.find_element_by_xpath("/html/body/div[6]/div[3]/div/div[1]/div[1]/div/div[1]/div/div[2]/div[3]/div/div/div/button")
+            in_apply = self.driver.find_element_by_class_name("jobs-apply-button.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view")
+                                                             
             in_apply.click()
+            print("entering application")
         except NoSuchElementException:
             print('you aready applied to this job go to the next job ...')
             pass
@@ -111,8 +126,14 @@ class EasyApply:
         
         # try to submit application
         try:
-            submit = self.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/div/form/footer/div[3]/button")
-            submit.send_keys(Keys.RETURN)
+            time.sleep(2)
+            submit = self.driver.find_element_by_class_name("artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
+            submit.click()
+            time.sleep(2) 
+            finish = self.driver.find_element_by_class_name("artdeco-modal__dismiss.artdeco-button.artdeco-button--circleartdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view")
+            finish.click()
+            print("applied")
+
            # if it is not aviable discard application
         except NoSuchElementException:
             print("not direct application")
@@ -125,19 +146,32 @@ class EasyApply:
                 time.sleep(1)
             except NoSuchElementException:
                 pass
+        except AttributeError:
+            pass
+        except ElementNotInteractableException:
+            pass
+        except ElementClickInterceptedException:
+            time.sleep(10)
+            
+            discard_popup = self.driver.find_element_by_class_name("artdeco-toast-item__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--1.artdeco-button--tertiary.ember-view")
+            hover = ActionChains(self.driver).move_to_element(discard_popup)
+            hover.perform()
+            discard_popup.send_keys(Keys.RETURN)
+            print("discarding popup")
+            pass
     def close_connection(self):
         self.driver.close()
         
     def apply(self):
         self.driver.maximize_window()
         self.login()
-        time.sleep(5)
+        time.sleep(2)
         self.job_search()
-        time.sleep(5)
+        time.sleep(2)
         self.clickeasyapply()
-        time.sleep(5)
+        time.sleep(2)
         self.find_offers()
-        time.sleep(5)
+        time.sleep(2)
         self.close_connection()
                 
 
